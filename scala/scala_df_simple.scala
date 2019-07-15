43ae73d560117314bc65422d76d4989ea68e24af
Jeremy Lao <jeremylao12@yahoo.co.uk>
2019-07-15 06:59:41 -0400

    Update scala_df_simple.scala

--- scala/scala_df_simple.scala
+++ scala/scala_df_simple.scala
@@ -22,5 +22,7 @@ val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 
 val dataSet = sqlContext.read.format("csv").option("header", "true").option("inferSchema", "true").load("project/data/top_1000.csv")
 
+val dataForAnalysis = dataSet.select("loan_amount_000s","applicant_income_000s","state_name","state_abbr","respondent_id","purchaser_type_name","property_type_name","loan_type_name","loan_purpose_name","county_name","as_of_year","applicant_sex_name","applicant_race_name_1","applicant_ethnicity_name","agency_name","agency_abbr","action_taken_name")
+
 dataSet.saveAsTextFile("project/clean-data/data_for_analysis.csv")
 
