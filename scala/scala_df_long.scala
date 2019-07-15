51e816ec9a749868343724f4c7340ee6b1678658
Jeremy Lao <jeremylao12@yahoo.co.uk>
2019-07-15 06:25:42 -0400

    Update scala_df_long.scala

--- scala/scala_df_long.scala
+++ scala/scala_df_long.scala
@@ -18,8 +18,8 @@ import scala.util.Random
 import sqlContext.implicits._
 
 val data = sc.textFile("project/data/top_1000.csv")
-val header = data.first
-val data_filtered = data.filter(_(0) != header(0))
+//val header = data.first
+//val data_filtered = data.filter(_(0) != header(0))
 val data_split = data_filtered.map(line => line.split(",").mkString(","))
 
 val data_tuple = data_split.map(line => {
@@ -29,5 +29,9 @@ val data_tuple = data_split.map(line => {
 val output = data_tuple.map(values => values.toString).
    map(s=>s.substring(1,s.length-1))
    output.take(10).foreach(println)
-  
- output.saveAsTextFile("project/clean-data/data_for_analysis.csv")
+ 
+val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+
+val dataSet = sqlContext.read.format("csv").option("header", "true").option("inferSchema", "true").load("project/data/top_1000.csv")
+
+output.saveAsTextFile("project/clean-data/data_for_analysis.csv")
