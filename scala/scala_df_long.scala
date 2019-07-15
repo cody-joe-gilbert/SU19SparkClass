69c846a27f4eb57f2b3b6d4df30a12f22e97f0ca
Jeremy Lao <jeremylao12@yahoo.co.uk>
2019-07-15 06:24:58 -0400

    Update scala_df_long.scala

--- scala/scala_df_long.scala
+++ scala/scala_df_long.scala
@@ -1,3 +1,22 @@
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.feature.Normalizer
+import org.apache.spark.ml.feature.StandardScaler
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.ml.regression.LinearRegression
+import org.apache.spark.ml.tuning.{ParamGridBuilder, TrainValidationSplit}
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.regression.LinearRegressionModel
+import org.apache.spark.mllib.regression.LinearRegressionWithSGD
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.sql._
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.ListBuffer
+import scala.util.Random
+import sqlContext.implicits._
+
 val data = sc.textFile("project/data/top_1000.csv")
 val header = data.first
 val data_filtered = data.filter(_(0) != header(0))
