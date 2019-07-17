769464e88e93afc783ccd6b1d7c1f938e3e1f6f2
Jeremy Lao <jeremylao12@yahoo.co.uk>
2019-07-17 11:22:08 -0400

    Update scala_df_simple.scala

--- scala/scala_df_simple.scala
+++ scala/scala_df_simple.scala
@@ -41,34 +41,31 @@ def dataProfiling(spark : SparkSession, hdfsPath : String) = {
                          "agency_name","agency_abbr",
                          "action_taken_name")
   
-    //val dataSet = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load("project/data/HMDA_DATA_2007_2017.csv")
     println("===========================")
     println("Loan Amount - Maximum,Minimum")
     dataForAnalysis.agg(max(dataForAnalysis(dataForAnalysis.columns(0))), min(dataForAnalysis(dataForAnalysis.columns(0)))).show
     val mrLoanAmount = dataForAnalysis.groupBy("loan_amount_000s").count()
-    mrLoanAmount.coalesce(1).write.mode("overwrite").format("csv").save("loan-amount-dist")
-
-    //val dataForAnalysis = dataSet.select("loan_amount_000s","applicant_income_000s","state_name","state_abbr","respondent_id","purchaser_type_name","property_type_name","loan_type_name","lien_status_name","loan_purpose_name","county_name","as_of_year","applicant_sex_name","applicant_race_name_1","applicant_ethnicity_name","agency_name","agency_abbr","action_taken_name")
+    mrLoanAmount.coalesce(1).write.mode("overwrite").format("csv").save("/user/jjl359/project/clean-data/loan-amount-dist")
 
     //Maximum-Minimum for Loan amount
     println("===========================")
     println("Loan Amount - Maximum,Minimum")
     dataForAnalysis.agg(max(dataForAnalysis(dataForAnalysis.columns(0))), min(dataForAnalysis(dataForAnalysis.columns(0)))).show
     val mrLoanAmount = dataForAnalysis.groupBy("loan_amount_000s").count()
-    mrLoanAmount.coalesce(1).write.mode("overwrite").format("csv").save("loan-amount-dist")
+    mrLoanAmount.coalesce(1).write.mode("overwrite").format("csv").save("/user/jjl359/project/clean-data/loan-amount-dist")
 
     println("===========================")
     println("Applicant Income - Maximum,Minimum")
     dataForAnalysis.agg(max(dataForAnalysis(dataForAnalysis.columns(1))), min(dataForAnalysis(dataForAnalysis.columns(1)))).show
     val mrAppIncome = dataForAnalysis.groupBy("applicant_income_000s").count()
-    mrAppIncome.coalesce(1).write.mode("overwrite").format("csv").save("app-income-dist")
+    mrAppIncome.coalesce(1).write.mode("overwrite").format("csv").save("/user/jjl359/project/clean-data/app-income-dist")
 
     println("===========================")
     println("Distinct Actions Taken")
     val distinctActionsTaken = dataForAnalysis.select(dataForAnalysis("action_taken_name")).distinct
     distinctActionsTaken.collect().foreach(println)
     val mrDistinctActionsTaken = dataForAnalysis.groupBy("action_taken_name").count()
-    mrDistinctActionsTaken.coalesce(1).write.mode("overwrite").format("csv").save("distinct-actions")
+    mrDistinctActionsTaken.coalesce(1).write.mode("overwrite").format("csv").save("/user/jjl359/project/clean-data/distinct-actions")
 
 
     println("===========================")
@@ -76,7 +73,7 @@ def dataProfiling(spark : SparkSession, hdfsPath : String) = {
     val distinctPurchaserType = dataForAnalysis.select(dataForAnalysis("purchaser_type_name")).distinct
     distinctPurchaserType.collect().foreach(println)
     val mrDistinctPurchaserType = dataForAnalysis.groupBy("purchaser_type_name").count()
-    mrDistinctPurchaserType.coalesce(1).write.mode("overwrite").format("csv").save("distinct-purchaser-type")
+    mrDistinctPurchaserType.coalesce(1).write.mode("overwrite").format("csv").save("/user/jjl359/project/clean-data/distinct-purchaser-type")
 
 
     println("===========================")
@@ -84,7 +81,7 @@ def dataProfiling(spark : SparkSession, hdfsPath : String) = {
     val distinctPropertyType = dataForAnalysis.select(dataForAnalysis("property_type_name")).distinct
     distinctPropertyType.collect().foreach(println)
     val property_type_name = dataForAnalysis.groupBy("property_type_name").count()
-    property_type_name.coalesce(1).write.mode("overwrite").format("csv").save("property-type-name")
+    property_type_name.coalesce(1).write.mode("overwrite").format("csv").save("/user/jjl359/project/clean-data/property-type-name")
 
 
     println("===========================")
@@ -92,7 +89,7 @@ def dataProfiling(spark : SparkSession, hdfsPath : String) = {
     val distinctLoanType = dataForAnalysis.select(dataForAnalysis("loan_type_name")).distinct
     distinctLoanType.collect().foreach(println)
     val loan_type_name = dataForAnalysis.groupBy("loan_type_name").count()
-    loan_type_name.coalesce(1).write.mode("overwrite").format("csv").save("loan-type-name")
+    loan_type_name.coalesce(1).write.mode("overwrite").format("csv").save("/user/jjl359/project/clean-data/loan-type-name")
 
 
     println("===========================")
@@ -100,7 +97,7 @@ def dataProfiling(spark : SparkSession, hdfsPath : String) = {
     val distinctLoanPurpose = dataForAnalysis.select(dataForAnalysis("loan_purpose_name")).distinct
     distinctLoanPurpose.collect().foreach(println)
     val loan_purpose_name = dataForAnalysis.groupBy("loan_purpose_name").count()
-    loan_purpose_name.coalesce(1).write.mode("overwrite").format("csv").save("loan_purpose_name")
+    loan_purpose_name.coalesce(1).write.mode("overwrite").format("csv").save("/user/jjl359/project/clean-data/loan_purpose_name")
 
 
     println("===========================")
@@ -108,7 +105,7 @@ def dataProfiling(spark : SparkSession, hdfsPath : String) = {
     val applicant_race_name = dataForAnalysis.select(dataForAnalysis("applicant_race_name_1")).distinct
     applicant_race_name.collect().foreach(println)
     val applicant_race_name_1 = dataForAnalysis.groupBy("applicant_race_name_1").count()
-    applicant_race_name_1.coalesce(1).write.mode("overwrite").format("csv").save("applicant_race_name_1")
+    applicant_race_name_1.coalesce(1).write.mode("overwrite").format("csv").save("/user/jjl359/project/clean-data/applicant_race_name_1")
 
 
     println("===========================")
@@ -116,31 +113,29 @@ def dataProfiling(spark : SparkSession, hdfsPath : String) = {
     val applicant_ethnicity_name = dataForAnalysis.select(dataForAnalysis("applicant_ethnicity_name")).distinct
     applicant_ethnicity_name.collect().foreach(println)
     val applicant_ethnicity_name = dataForAnalysis.groupBy("applicant_ethnicity_name").count()
-    applicant_ethnicity_name.coalesce(1).write.mode("overwrite").format("csv").save("applicant_ethnicity_name")
+    applicant_ethnicity_name.coalesce(1).write.mode("overwrite").format("csv").save("/user/jjl359/project/clean-data/applicant_ethnicity_name")
 
     println("===========================")
     println("Applicant Gender")
     val applicant_sex_name = dataForAnalysis.groupBy("applicant_sex_name").count()
-    applicant_sex_name.coalesce(1).write.mode("overwrite").format("csv").save("applicant_sex_name")
+    applicant_sex_name.coalesce(1).write.mode("overwrite").format("csv").save("/user/jjl359/project/clean-data/applicant_sex_name")
 
 
     println("===========================")
     println("Applicant Lender")
     val respondent_id = dataForAnalysis.groupBy("as_of_year","respondent_id").count()
-    respondent_id.coalesce(1).write.mode("overwrite").format("csv").save("respondent_id")
+    respondent_id.coalesce(1).write.mode("overwrite").format("csv").save("/user/jjl359/project/clean-data/respondent_id")
 
     println("===========================")
     println("County")
     val county_name = dataForAnalysis.groupBy("as_of_year","state_abbr","county_name").count()
-    county_name.coalesce(1).write.mode("overwrite").format("csv").save("state_county_occurrence")
+    county_name.coalesce(1).write.mode("overwrite").format("csv").save("/user/jjl359/project/clean-data/state_county_occurrence")
 
 }
 
 
 val spark = SparkSession.builder().appName("DataProfiling").getOrCreate()
-val path = "project/data/HMDA_2007_to_2017.csv"
-val smallFilePath = "project/data/top_1000.csv"
-dataProfiling(spark,smallFilePath)
-
-//dataSet.saveAsTextFile("project/clean-data/")
+val path = "/user/jjl359/project/data/HMDA_2007_to_2017.csv"
+val smallFilePath = "/user/jjl359/project/data/top_1000.csv"
+dataProfiling(spark,path)
 
