56c9157cd34fe6be931650a77f039c5a5a3e54f9
Jeremy James Lao <jjl359@login-1-1.local>
2019-07-17 19:45:33 -0400

    adding pom.xml and expanding folder structure in src for spark-submit jobs/code

---
+++ scala-repl-scripts/archive/scala_df_long.scala
@@ -0,0 +1,36 @@
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.feature.Normalizer
+import org.apache.spark.ml.feature.StandardScaler
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.ml.regression.LinearRegression
+import org.apache.spark.ml.tuning.{ParamGridBuilder, TrainValidationSplit}
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.regression.LinearRegressionModel
+import org.apache.spark.mllib.regression.LinearRegressionWithSGD
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.sql._
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.ListBuffer
+import scala.util.Random
+import sqlContext.implicits._
+
+val data = sc.textFile("project/data/top_1000.csv")
+val data_split = data.map(line => line.split(",").mkString(","))
+
+val data_tuple = data_split.map(line => {
+   val lines = line.split(',')
+  (lines(6), lines(8), lines(9), lines(10), lines(12), lines(13), lines(14), lines(18), lines(19), lines(26), lines(35), lines(37), lines(42), lines(43), lines(44), lines(45), lines(46))
+  })
+val output = data_tuple.map(values => values.toString).
+   map(s=>s.substring(1,s.length-1))
+   output.take(10).foreach(println)
+ 
+output.saveAsTextFile("project/clean-data/data_for_analysis.csv")
+
+val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+
+val dataSet = sqlContext.read.format("csv").option("header", "true").option("inferSchema", "true").load("project/clean-data/part*")
+
