6242d650291a5a19c625911ee07c486b9262a4e1
jeremylao <jeremylao12@yahoo.co.uk>
2019-07-18 23:44:27 -0400

    update

--- src/scraping/concatenate_hmda_codes.bash
+++ src/scraping/concatenate_hmda_codes.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-{ cat hmda_2007_nationwide_all-records_codes.csv; sed '1d' hmda_2008_nationwide_all-records_codes.csv; sed '1d' hmda_2009_nationwide_all-records_codes.csv; sed '1d' hmda_2010_nationwide_all-records_codes.csv; sed '1d' hmda_2011_nationwide_all-records_codes.csv; sed '1d' hmda_2012_nationwide_all-records_codes.csv; sed '1d' hmda_2013_nationwide_all-records_codes.csv; sed '1d' hmda_2014_nationwide_all-records_codes.csv; sed '1d' hmda_2015_nationwide_all-records_codes.csv; sed '1d' hmda_2016_nationwide_all-records_codes.csv;  sed '1d' hmda_2017_nationwide_all-records_codes.csv;} > HMDA_2007_to_2017_codes.csv
+{ cat hmda_2007.csv; sed '1d' hmda_2008.csv; sed '1d' hmda_2009.csv; sed '1d' hmda_2010.csv; sed '1d' hmda_2011.csv; sed '1d' hmda_2012.csv; sed '1d' hmda_2013.csv; sed '1d' hmda_2014.csv; sed '1d' hmda_2015.csv; sed '1d' hmda_2016.csv;  sed '1d' hmda_2017.csv;} > HMDA_2007_to_2017_codes.csv
