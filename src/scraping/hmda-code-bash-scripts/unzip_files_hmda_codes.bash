14c157f1e846d82bae83540b7b040dc8d075c2fe
jeremylao <jeremylao12@yahoo.co.uk>
2019-07-18 23:07:59 -0400

    update

---
+++ src/scraping/hmda-code-bash-scripts/unzip_files_hmda_codes.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+mkdir /scratch/"$@"/hmda_code_files
+mkdir /scratch/"$@"/hmda_code_files/data-files
+
+for i in {2007..2017};
+do
+  prefix="hmda_"
+  ext=".zip"
+  csv=".csv"
+
+  echo $prefix$i$ext
+
+  unzip -d /scratch/"$@"/hmda_code_files/data-files/ /scratch/"$@"/hmda_code_files/zip-files/$prefix$i$ext
+  mv /scratch/"$@"/hmda_code_files/data-files/hmda_lar.csv /scratch/"$@"/hmda_code_files/data-files/$prefix$i$csv
+
+done
