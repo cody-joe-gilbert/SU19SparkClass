d3f7f24f45d99f09717c8375e228b26a0738ed4d
Jeremy Lao <jeremylao12@yahoo.co.uk>
2019-07-18 23:40:34 -0400

    Update unzip_files_hmda_codes.bash

--- src/scraping/unzip_files_hmda_codes.bash
+++ src/scraping/unzip_files_hmda_codes.bash
@@ -8,10 +8,11 @@ do
   prefix="hmda_"
   ext=".zip"
   csv=".csv"
-
+  end_name="_nationwide_all-records_codes.csv"
+  
   echo $prefix$i$ext
 
   unzip -d /scratch/"$@"/hmda_code_files/data-files/ /scratch/"$@"/hmda_code_files/zip-files/$prefix$i$ext
-  mv /scratch/"$@"/hmda_code_files/data-files/hmda_$i_nationwide_all-records_codes.csv /scratch/"$@"/hmda_code_files/data-files/$prefix$i$csv
+  mv /scratch/"$@"/hmda_code_files/data-files/hmda_$i$end_name /scratch/"$@"/hmda_code_files/data-files/$prefix$i$csv
 
 done
