a29f0b05a86d5f608ae7b01b930ed80afd6ea030
Fang Han <hanfangviolin@gmail.com>
2019-07-26 11:55:36 -0400

    creating modeling landing page

--- presentation/website/entry.py
+++ presentation/website/entry.py
@@ -34,21 +34,15 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         flash(f'User profile created for {form.email.data}!', 'success')
-        return redirect(url_for('home'))
+        return redirect(url_for('modeling'))
     return render_template('findLender.html', title='Register', form=form)
 
-"""
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
-"""
+
+@app.route("/modeling", methods=['GET', 'POST'])
+def modeling():
+
+    return render_template('modeling.html', title='Modeling')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
