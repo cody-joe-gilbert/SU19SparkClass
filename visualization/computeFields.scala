3846718c0030c0d22924f94af9158677eaf2bd1f
Fang Han <hanfangviolin@gmail.com>
2019-08-01 18:58:30 -0400

    added csvs

--- visualization/computeFields.scala
+++ visualization/computeFields.scala
@@ -29,7 +29,7 @@ val joined = sex_count.join(tmp,
 // ---- USER DEFINED FUNCTION TO TAKE DIVISION
 val myUDF = udf((num: Long, denom: Long) => (num.toDouble/denom.toDouble))
 val sex_out = joined.withColumn("denial", myUDF(col("count"), col("sum"))).select("year", "sex", "denial")
-sex_out.write.
+sex_out.coalesce(1).write.
     mode("overwrite").
     option("header","true").
     format("csv").
@@ -48,7 +48,7 @@ val joined = race_count.filter($"action" === "deny").
                        select("year", "race", "count", "sum")
 
 val race_out = joined.withColumn("denial", myUDF(col("count"), col("sum"))).select("year", "race", "denial")
-race_out.write.
+race_out.coalesce(1).write.
     mode("overwrite").
     option("header","true").
     format("csv").
@@ -65,7 +65,7 @@ val joined = eth_count.filter($"action" === "deny").
                        select("year", "eth", "count", "sum")
 
 val eth_out = joined.withColumn("denial", myUDF(col("count"), col("sum"))).select("year", "eth", "denial")
-eth_out.write.
+eth_out.coalesce(1).write.
     mode("overwrite").
     option("header","true").
     format("csv").
@@ -95,14 +95,14 @@ val numerical = income.rdd.
 +-------+------------------+
 */
 
-//numerical.stat.approxQuantile("income", Array(0.20, 0.40, 0.60, 0.80, 0.90), 0.1)
-// res61: Array[Double] = Array(58.0, 77.0, 128.0, 186.0, 610715.0)
+numerical.stat.approxQuantile("income", Array(0.20, 0.40, 0.60, 0.80, 0.90), 0.1)
+//Array[Double] = Array(58.0, 77.0, 128.0, 186.0, 610715.0)
 
 numerical.stat.approxQuantile("income", Array(0.05, 0.1, 0.15, 0.20, 0.25, 0.3,0.35, 0.40, 0.45, 0.5, 0.55, 0.60,0.65, 0.7, 0.75,0.80,0.85, 0.90, 0.95, 1.0), 0.1)
 //Array(29.0, 35.0, 40.0, 45.0, 50.0, 55.0, 60.0, 67.0, 72.0, 77.0, 85.0, 92.0, 100.0, 112.0, 122.0, 138.0, 161.0, 196.0, 300.0, 610715.0)
 
- numerical.stat.approxQuantile("income", Array(0.05, 0.1, 0.15, 0.20, 0.25, 0.3,0.35, 0.40, 0.45, 0.5, 0.55, 0.60,0.65, 0.7, 0.75,0.80,0.85, 0.90, 0.95, 1.0), 0.001)
-//res64: Array[Double] = Array(26.0, 33.0, 38.0, 43.0, 48.0, 54.0, 59.0, 64.0, 70.0, 76.0, 83.0, 90.0, 98.0, 108.0, 120.0, 134.0, 154.0, 186.0, 257.0, 610715.0)
+numerical.stat.approxQuantile("income", Array(0.05, 0.1, 0.15, 0.20, 0.25, 0.3,0.35, 0.40, 0.45, 0.5, 0.55, 0.60,0.65, 0.7, 0.75,0.80,0.85, 0.90, 0.95, 1.0), 0.001)
+//Array[Double] = Array(26.0, 33.0, 38.0, 43.0, 48.0, 54.0, 59.0, 64.0, 70.0, 76.0, 83.0, 90.0, 98.0, 108.0, 120.0, 134.0, 154.0, 186.0, 257.0, 610715.0)
 
 
 
